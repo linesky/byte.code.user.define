97a02ab0842082fb80a7844cb32b7b976254b0d9
sky line <32512284+linesky@users.noreply.github.com>
2024-11-05 16:46:38 +0000

    Add files via upload

--- decompiler.py
+++ decompiler.py
@@ -15,11 +15,14 @@ list1=txts.split("\n")
 txts=""
 onon=False
 for n in list1:
-    
+    spacess=n.strip()!=""
+    pointss=True
+    if not(n.find(".class")>-1 or n.find(".method")>-1 or n.find(" .entrypoint")>-1):
+         pointss=(n.find(".")!=0)
     ill=n.find("//")
     if ill>-1:
         n=n[:ill]
-    if onon:
+    if onon and spacess and pointss:
         txts=txts+n+"\n"
     if n.find(" "+files)>-1:
         onon=True
