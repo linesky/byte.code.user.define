87a4bb4d14344d015fcb48eabe960b9bc9fedaf6
sky line <32512284+linesky@users.noreply.github.com>
2024-11-06 08:31:14 +0000

    Add files via upload

--- decompiler.py
+++ decompiler.py
@@ -16,9 +16,65 @@ txts=""
 onon=False
 for n in list1:
     spacess=n.strip()!=""
+    
+    if n.find(".class")>-1:
+        t=""
+        ill=n.find(".")
+        if ill>-1:
+            t=n[:ill]
+        
+        ill=n.find("<")
+        tt=""
+        if ill>-1:
+            tt=n[ill+1:]
+        ill=n.find(">")
+        if ill>-1:
+            tt=tt[:ill]
+        n=t+"class"+tt 
+    if n.find(".method")>-1:
+        t=""
+        ill=n.find(".")
+        if ill>-1:
+            t=n[:ill]
+
+        ill=n.find("<")
+        tt=""
+        if ill>-1:
+            tt=n[ill+1:]
+        ill=tt.find(">")
+        if ill>-1:
+            tt=tt[:ill]
+        n=t+"int "+tt 
+
+        
+    if n.find(".entrypoint")>-1:
+        t=""
+        ill=n.find(".")
+        if ill>-1:
+            t=n[:ill]
+        n=t+"entrypoint:"
+    if n.find("call ")>-1:
+        t=""
+        ill=n.find("call")
+        if ill>-1:
+            t=n[:ill]
+        tt=""
+        ill=n.find("::")
+        if ill>-1:
+            tt=n[ill+2:]
+        n=t+"call "+tt
     pointss=True
-    if not(n.find(".class")>-1 or n.find(".method")>-1 or n.find(" .entrypoint")>-1):
-         pointss=(n.find(".")!=0)
+    if n.find(".custom")>-1:
+        pointss=False
+    if n.find(" nop")>-1:
+        pointss=False
+ 
+    if n.find("extends")>-1:
+        pointss=False
+    if n.find("instance")>-1:
+        pointss=False
+    if n.find(".")==0:
+        pointss=False
     ill=n.find("IL_")
     if ill>-1:
         n=n[:ill]+n[ill+9:]
